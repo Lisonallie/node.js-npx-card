b834db92164d5bc82cd0b9d87df49bb6c4e8c186
Lisonallie <allisonvanlinden@gmail.com>
2019-08-16 13:53:08 +0200

    try to get code working

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-const boxen = require('boxen');
-const terminalLink = require('terminal-link');
-const chalk = require('chalk');
-const gradient = require('gradient-string');
+const boxen = require('boxen')
+const chalk = require('chalk')
+const gradient = require('gradient-string')
+const fs = require('fs')
 
 const options = {
     padding: 1,
     margin: 1,
     borderStyle: 'round'
-  }
+}
 
-  const cardData = {
-    name: gradient.pastel('              Allison Van Linden'),
+const cardData = {
+    name: (gradient.pastel('              Allison Van Linden')),
     handle: chalk.white('npx card'),
     work: chalk.white('Junior Web Developer at BeCode.org'),
     github: chalk.gray('https://github.com/') + chalk.green('lisonallie'),
@@ -19,15 +19,44 @@ const options = {
     behance: chalk.cyan('https://behance.net/') + chalk.red('AllisonVanLinden'),
     facebook: chalk.red('https://facebook.com/') + chalk.white('LyssiArt'),
     instagram: chalk.gray('https://instagram/com/') + chalk.blue('lyssi_artist'),
-    labelWork: chalk.white.bold('       Work:'),
-    labelGitHub: chalk.white.bold('     GitHub:'),
-    labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-    labelWeb: chalk.white.bold('        Web:'),
-    labelCard: chalk.white.bold('       Card:')
-  }
+    program: chalk.cyan('HTML') + chalk.red('CSS') + chalk.white('JavaScript') + chalk.gray('PHP') + chalk.green('SQL') + chalk.blue('node.js'),
+    language: chalk.white('Fluent:') + chalk.green('English, Dutch') + chalk.white('Beginner:') + chalk.cyan('French, Japanese'),
+    labelWork: chalk.white('       Job Title:'),
+    labelGitHub: chalk.white('     GitHub:'),
+    labelLinkedIn: chalk.white('   LinkedIn:'),
+    labelBehance: chalk.white('        Behance:'),
+    labelFacebook: chalk.white('       Facebook:'),
+    labelInstagram: chalk.white('      Instagram:'),
+    labelProgramming: chalk.white('    Programming Languages:'),
+    labelLanguages: chalk.white('      Languages:')
+}
 
+// Actual strings we're going to output
+const newline = '\n';
+const heading = `${cardData.name} / ${cardData.handle}`
+const working = `${cardData.labelWork}  ${cardData.work}`
+const githubbing = `${cardData.labelGitHub}  ${cardData.github}`
+const linkedining = `${cardData.labelLinkedIn}  ${cardData.linkedin}`
+const behanceing = `${cardData.labelBehance}  ${cardData.behance}`
+const facebooking = `${cardData.labelFacebook}  ${cardData.facebook}`
+const instagramming = `${cardData.labelInstagram}  ${cardData.instagram}`
+const programming = `${cardData.labelProgramming}  ${cardData.programming}`
+const languageing = `${cardData.labelLanguages}  ${cardData.languages}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // cardData.name + cardData.handle
+    newline + newline + // Add one whole blank line
+    working + newline + newline + // cardData.labelWork + cardData.work
+    githubbing + newline + // cardData.labelTwitter + cardData.twitter
+    linkedining + newline + // cardData.labelnpm + cardData.npm
+    behanceing + newline + // cardData.labelGitHub + cardData.github
+    facebooking + newline + // cardData.labelLinkedIn + cardData.linkedin
+    instagramming + newline + newline + // cardData.labelWeb + cardData.web
+    programming + newline + //cardData.labelProgramming + cardData.program
+    languageing + newline + //cardData.labelLanguages + cardData.language
+
+console.log(boxen('output', options));
+
+// fs.writeFileSync(path.join(__filename, ''), chalk.green(boxen(output, options)))
 
 // let coolGradient = gradient(['#FF0000', '#00FF00', '#0000FF']);
-
-
-// console.log(gradient.pastel("Allison Van Linden"));

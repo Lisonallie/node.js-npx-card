ae0bb1890380df1d5d008baeb63c8d9435d5920f
Lisonallie <allisonvanlinden@gmail.com>
2019-08-16 14:42:47 +0200

    try to get code to work

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+'use strict'
+
 const boxen = require('boxen')
 const chalk = require('chalk')
 const gradient = require('gradient-string')
-const fs = require('fs')
+
 
 const options = {
     padding: 1,
@@ -11,7 +15,15 @@ const options = {
 }
 
 const cardData = {
-    name: (gradient.pastel('              Allison Van Linden')),
+    labelWork: chalk.white('       Job Title:'),
+    labelGitHub: chalk.white('     GitHub:'),
+    labelLinkedIn: chalk.white('   LinkedIn:'),
+    labelBehance: chalk.white('        Behance:'),
+    labelFacebook: chalk.white('       Facebook:'),
+    labelInstagram: chalk.white('      Instagram:'),
+    labelProgramming: chalk.white('    Programming Languages:'),
+    labelLanguages: chalk.white('      Languages:'),
+    name: gradient.pastel('              Allison Van Linden'),
     handle: chalk.white('npx card'),
     work: chalk.white('Junior Web Developer at BeCode.org'),
     github: chalk.gray('https://github.com/') + chalk.green('lisonallie'),
@@ -20,19 +32,12 @@ const cardData = {
     facebook: chalk.red('https://facebook.com/') + chalk.white('LyssiArt'),
     instagram: chalk.gray('https://instagram/com/') + chalk.blue('lyssi_artist'),
     program: chalk.cyan('HTML') + chalk.red('CSS') + chalk.white('JavaScript') + chalk.gray('PHP') + chalk.green('SQL') + chalk.blue('node.js'),
-    language: chalk.white('Fluent:') + chalk.green('English, Dutch') + chalk.white('Beginner:') + chalk.cyan('French, Japanese'),
-    labelWork: chalk.white('       Job Title:'),
-    labelGitHub: chalk.white('     GitHub:'),
-    labelLinkedIn: chalk.white('   LinkedIn:'),
-    labelBehance: chalk.white('        Behance:'),
-    labelFacebook: chalk.white('       Facebook:'),
-    labelInstagram: chalk.white('      Instagram:'),
-    labelProgramming: chalk.white('    Programming Languages:'),
-    labelLanguages: chalk.white('      Languages:')
+    language: chalk.white('Fluent:') + chalk.green('English, Dutch') + chalk.white('Beginner:') + chalk.cyan('French, Japanese')
 }
 
+
 // Actual strings we're going to output
-const newline = '\n';
+const newline = '\n'
 const heading = `${cardData.name} / ${cardData.handle}`
 const working = `${cardData.labelWork}  ${cardData.work}`
 const githubbing = `${cardData.labelGitHub}  ${cardData.github}`
@@ -40,11 +45,11 @@ const linkedining = `${cardData.labelLinkedIn}  ${cardData.linkedin}`
 const behanceing = `${cardData.labelBehance}  ${cardData.behance}`
 const facebooking = `${cardData.labelFacebook}  ${cardData.facebook}`
 const instagramming = `${cardData.labelInstagram}  ${cardData.instagram}`
-const programming = `${cardData.labelProgramming}  ${cardData.programming}`
-const languageing = `${cardData.labelLanguages}  ${cardData.languages}`
+const programming = `${cardData.labelProgramming}  ${cardData.program}`
+const languageing = `${cardData.labelLanguages}  ${cardData.language}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // cardData.name + cardData.handle
+const cardText = heading + // cardData.name + cardData.handle
     newline + newline + // Add one whole blank line
     working + newline + newline + // cardData.labelWork + cardData.work
     githubbing + newline + // cardData.labelTwitter + cardData.twitter
@@ -55,7 +60,7 @@ const output = heading + // cardData.name + cardData.handle
     programming + newline + //cardData.labelProgramming + cardData.program
     languageing + newline + //cardData.labelLanguages + cardData.language
 
-console.log(boxen('output', options));
+chalk.green(boxen(`cardText`, options))
 
 // fs.writeFileSync(path.join(__filename, ''), chalk.green(boxen(output, options)))
 
